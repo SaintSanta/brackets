c13370486b2ad9c5ac25fb6e4733d3b28c1ac265
SaintSanta <aleks2art@gmail.com>
2020-10-25 21:25:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (var i = str.length; i >= 0; i--) {
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            var same = bracketsConfig[j].join('');
+            if (str.includes(same)) {
+                str = str.replace(same, '');
+            }
+        }
+        if (str.length === 0) {
+            return true;
+        }
+    }
+    return str === "";
 }
